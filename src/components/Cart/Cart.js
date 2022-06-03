c6e2c1cf9e21fb2b4e2680f79e4ea7f080c7878e
Luciano Vannelli <lucianovannelli@gmail.com>
2022-06-03 14:47:40 -0300

    Cart implementation

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,21 @@
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[{ id: "c1", name: "sushi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <div>
+      {cartItems}
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Cart;
